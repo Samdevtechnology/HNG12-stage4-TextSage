c668afa92530736aad9510c814f10fed0761f602
Samdevtechnology <Samdevtechnology@gmail.com>
2025-02-21 09:41:37 +0100

    Refactor HomePage component: update imports, rename Sidebar, and enhance layout responsiveness

--- src/app/(Home)/Home.jsx
+++ src/app/(Home)/Home.jsx
@@ -1,16 +1,17 @@
-"use client";
-
-import ChatHistory from "@/components/ChatFlow";
+import ChatFlow from "@/components/common/ChatFlow";
 import Header from "@/components/common/Header";
-import ChatInput from "@/components/form/Input";
-import Sidebar from "@/components/History";
+import ChatInput from "@/components/form/ChatInput";
+import SideBar from "@/components/common/SideBar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { useStore } from "@/store/chat";
 import { useEffect, useState } from "react";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { cn } from "@/lib/utils";
 
 const HomePage = () => {
   const currentChat = useStore((state) => state.getCurrentChat());
   const hasMessages = !currentChat || currentChat.messages.length > 0;
+  const isMobile = useIsMobile();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -27,7 +28,7 @@ const HomePage = () => {
 
   return (
     <SidebarProvider defaultOpen={false}>
-      <Sidebar />
+      <SideBar />
       <SidebarInset className="bg-[url('/assets/noise.png')] flex flex-col h-full min-h-screen">
         <Header />
         <div className="flex flex-col flex-1 max-w-3xl w-full mx-auto rounded-xl">
@@ -35,7 +36,7 @@ const HomePage = () => {
             // Layout when there are messages
             <div className="flex-1 flex flex-col h-full">
               <div className="flex-1 overflow-y-auto p-6">
-                <ChatHistory />
+                <ChatFlow />
               </div>
               <div className="sticky bottom-0 p-2 pb-0">
                 <ChatInput />
@@ -43,7 +44,12 @@ const HomePage = () => {
             </div>
           ) : (
             // Centered layout when empty
-            <div className="flex-1 flex flex-col h-full justify-center items-center p-4">
+            <div
+              className={cn(
+                `flex-1 flex flex-col h-full justify-center items-center p-4`,
+                isMobile && "justify-end"
+              )}
+            >
               <div className="w-full max-w-2xl">
                 <ChatInput />
               </div>
